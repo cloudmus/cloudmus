df12f7e65bb0b6be09a4d8e6f06bf979587bce39
Navrocky Vladislav <navrocky.vlad@gmail.com>
2015-07-02 00:29:56 +0300

    * Work on about and options dialogs

--- app/aboutdialog.cpp
+++ app/aboutdialog.cpp
@@ -1,11 +1,19 @@
 #include "aboutdialog.h"
 #include "ui_aboutdialog.h"
 
-AboutDialog::AboutDialog(QWidget *parent) :
+AboutDialog::AboutDialog(QWidget* parent) :
     QDialog(parent),
     ui(new Ui::AboutDialog)
 {
     ui->setupUi(this);
+
+    QPalette transpPalette;
+    transpPalette.setColor(QPalette::Base, Qt::transparent);
+    transpPalette.setColor(QPalette::Text, transpPalette.color(QPalette::WindowText));
+    ui->aboutBrowser->setPalette(transpPalette);
+    ui->whatsNewBrowser->setPalette(transpPalette);
+    ui->authorsBrowser->setPalette(transpPalette);
+
 }
 
 AboutDialog::~AboutDialog()
